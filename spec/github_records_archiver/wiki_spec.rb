4661aad32660ddc7a796b027ca5461209ce84c38
Ben Balter <ben.balter@github.com>
2018-03-30 12:42:05 -0400

    correct rubocop offenses

--- spec/github_records_archiver/wiki_spec.rb
+++ spec/github_records_archiver/wiki_spec.rb
@@ -1,29 +1,29 @@
 RSpec.describe GitHubRecordsArchiver::Wiki do
-  let(:repo) { "balter-test-org/some-repo" }
+  let(:repo) { 'balter-test-org/some-repo' }
   subject { described_class.new(repo) }
 
   before do
     stub_api_request('repos/balter-test-org/some-repo')
   end
 
-  it "stores the repo" do
+  it 'stores the repo' do
     expect(subject.repository).to be_a(GitHubRecordsArchiver::Repository)
     expect(subject.repository.name).to eql(repo)
   end
 
-  context "when initialized with a Repository object" do
+  context 'when initialized with a Repository object' do
     subject do
       repository = GitHubRecordsArchiver::Repository.new(repo)
       described_class.new(repository)
     end
 
-    it "stores the repo" do
+    it 'stores the repo' do
       expect(subject.repository).to be_a(GitHubRecordsArchiver::Repository)
       expect(subject.repository.name).to eql(repo)
     end
   end
 
-  it "builds the repo dir" do
+  it 'builds the repo dir' do
     path = File.expand_path "../../archive/#{repo}/wiki", __dir__
     expect(subject.repo_dir).to eql(path)
   end
